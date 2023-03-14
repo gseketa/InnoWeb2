a8a9ffb8d396508125142a96fa0943a32341fc87
Goran <gseketa@gmail.com>
2023-03-14 16:08:38 +0100

    works again with build

--- src/assets/theme/functions/boxShadow.js
+++ src/assets/theme/functions/boxShadow.js
@@ -17,11 +17,13 @@ Coded by www.creative-tim.com
   The boxShadow() function helps you to create a box shadow for an element
  */
 
+/* eslint-disable */
+
 // Material Kit 2 React helper functions
 import rgba from "assets/theme/functions/rgba";
 import pxToRem from "assets/theme/functions/pxToRem";
 
-function boxShadow(color, opacity, offset = [], radius = [], inset = "") {
+function boxShadow(offset = [], radius = [], color, opacity, inset = "") {
   const [x, y] = offset;
   const [blur, spread] = radius;
 
