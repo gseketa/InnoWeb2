adc1a849a4f2436f9073ec301b11a1e117909bcc
Goran <gseketa@gmail.com>
2023-09-12 15:49:37 +0200

    ignore ReactDom error in index.js

--- src/index.js
+++ src/index.js
@@ -18,9 +18,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
+// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
+
+
+
+
+
+
+
+
